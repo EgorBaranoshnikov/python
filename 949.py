034a373a715037e21bdb044e190bd3401799d8af
Egor <egor.baranoshnikov@gmail.com>
2024-03-08 21:11:30 +0300

    новые задачи

--- 949.py
+++ 949.py
@@ -5,13 +5,19 @@ ind_ch = int(mas[0])
 ch1 = int(mas[1])
 ch2 = int(mas[2])
 
-
-bol = ch2
-men = ch1
-for i in range(ind_ch - 2):
+for i in range(1):
+    if ind_ch == 1:
+        print(ch1, ch2)
+        break
+    if ind_ch == 2:
+        print(ch2 - ch1, ch1)
+        break
+    bol = ch2
+    men = ch1
+    for i in range(ind_ch - 2):
+        a = bol - men
+        bol = men
+        men = a
+    d = a
     a = bol - men
-    bol = men
-    men = a
-d = a
-a = bol - men
-print(d, a)             
+    print(a, d)             
